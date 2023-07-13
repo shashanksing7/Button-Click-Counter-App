749860031530519c7f414b1712f614ab98d70fb7
noobtopy <68452635+sassank88888@users.noreply.github.com>
2023-07-13 15:30:19 +0530

    second commit

--- app/src/main/java/www/zentorc/simplehelloworldapp/MainActivity.java
+++ app/src/main/java/www/zentorc/simplehelloworldapp/MainActivity.java
@@ -1,14 +1,40 @@
 package www.zentorc.simplehelloworldapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    AppCompatButton button;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+
+        button=findViewById(R.id.button);
+        textView=findViewById(R.id.textview);
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+
+                int data=Integer.parseInt(textView.getText().toString());
+                data=data+1;
+                textView.setText(String.valueOf(data));
+            }
+        });
     }
+
+
 }
